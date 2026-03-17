4ca4d4a7410b8c298ca3be4f03b58ac5e8afae6a
Bui Quoc Toan <toanbq@gem-corp.global>
2026-03-17 17:27:18 +0700

    study while for

---
+++ tap-js/bai-tap-13.js
@@ -0,0 +1,86 @@
+// let i = 1;
+// while (i <= 5){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 5;
+// while (i > 0){
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 2;
+// while(i <= 10){
+//     console.log(i);
+//     i+=2;
+// }
+
+// let i = 1;
+// while (i <= 10){
+//     console.log(i + 1);
+//     i+=2;
+// }
+
+// for (let i = 5; i > 0; i-- ){
+//     console.log(i)
+// }
+
+// let i = 1;
+// while(i <= 5){
+//     let j = 1;
+//     let row = "";
+
+//     while(j <= i){
+//         row = row + i;
+//         j++;
+//     }
+//     console.log(row);
+//     i++;
+// }
+
+// for (i = 1; i<= 5; i++ ){
+//     let row = "";
+//     for(let j = 1; j <= i; j++){
+//         row += i;
+//     }
+//     console.log(row);
+// }
+
+// let i = 1;
+// while(i <= 5){
+//     let j = 1;
+//     let row = "";
+
+//     while(j <= i){
+//         row = row + j;
+//         j++;
+//     }
+//     console.log(row);
+//     i++;
+// }
+
+// for (let i = 1; i<= 5; i++ ){
+//     let row = "";
+//     for(let j = 1; j <= i; j++){
+//         row += j;
+//     }
+//     console.log(row);
+// }
+
+// Bảng cửu chương
+
+// for(let j = 1; j <= 10; j++){
+//     let i = 2;
+//     console.log(i + " x " + j + " = " + (i*j));
+    
+// }
+
+// let i = 2;
+// let j = 1;
+
+// while(j <= 10){
+//     console.log(i + " x " + j + " = " + (i * j));
+//     j++;
+// }
+
