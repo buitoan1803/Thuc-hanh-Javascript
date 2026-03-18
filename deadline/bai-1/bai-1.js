5cfaf47ef691cba559482c8c0597fa5b99c44757
Bui Quoc Toan <toanbq@gem-corp.global>
2026-03-18 14:30:09 +0700

    update price FOR WHILE

--- deadline/bai-1/bai-1.js
+++ deadline/bai-1/bai-1.js
@@ -1,21 +1,33 @@
 const products = [
  {id: 1, name: 'Laptop', price: 1000, quantity: 3},
  {id: 2, name: 'Mouse', price: 20, quantity: 1},
+ {id: 3, name: 'Keyboard', price: 80, quantity: 5},
+ {id: 3, name: 'Keyboard', price: 80, quantity: 5},
  {id: 3, name: 'Keyboard', price: 80, quantity: 5}
 ]
 
 let total = 0;
 
-if (products[0].quantity > 2){
-    total += products[0].price * products[0].quantity;
+for( let i = 0 ; i < products.length; i++){
+    if(products[i].quantity > 2 ){
+        total += products[i].price * products[i].quantity;
+    }
+    
 }
+console.log(total)
+// let total = 0;
 
-if (products[1].quantity > 2){
-    total += products[1].price * products[1].quantity;
-}
+// if (products[0].quantity > 2){
+//     total += products[0].price * products[0].quantity;
+// }
 
-if (products[2].quantity > 2){
-    total += products[2].price * products[2].quantity;
-}
+// if (products[1].quantity > 2){
+//     total += products[1].price * products[1].quantity;
+// }
 
-console.log(total);
+// if (products[2].quantity > 2){
+//     total += products[2].price * products[2].quantity;
+// }
+
+// console.log(total);
+
--- tap-js/bai-tap-13.js
+++ tap-js/bai-tap-13.js
@@ -84,3 +84,249 @@
 //     j++;
 // }
 
+// for(let i = 2; i<= 9; i++){
+//     for( let j = 1; j <= 10; j++){
+//     console.log(i + " x " + j + " = " + (i*j));
+//     }
+//     console.log("====================");
+// }
+
+// let i = 2;
+// while(i <=9 ){
+
+//     let j = 1;
+//     while( j <=10 ){
+//         console.log(i + " x " + j + " = " + (i * j));
+//         j++
+//     }
+//     console.log("===========");
+//     i++
+// }
+
+// let i = 5;
+
+// while(i <= 5){
+//     let j = 1;
+//     let row = "";
+
+//     while(j <= i){
+//         row = row + "*";
+//         j++
+//     }
+
+//     console.log(row);
+//     i--
+// }
+
+// for(let i = 5; i <= 5; i++){
+//     let row = "";
+    
+//     for (let j = 1; j <= i; j++ ){
+        
+//         row = row + "*"
+//     }
+//     console.log(row);
+// }
+
+
+// let i = 5;
+// while (i > 0 ) {
+//     let j = 1;
+//     let row = "";
+
+//     while(j <= i){
+//         row += "*";
+//         j++;
+//     }
+    
+//     console.log(row);
+//     i--;
+// }
+
+// for(let i = 5; i > 0; i--){
+//     let row = "";
+//     for(let j = 1; j <= i; j++){
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+// for (let i = 1; i <= 5; i++ ){
+//     let row = "";
+    
+//     for(let j = 1; j <= 5 - i; j++){
+//         row += " ";
+//     }
+
+//     for(let j = 1; j <= i; j++){
+//         row += "*";
+//     }
+
+//     console.log(row);
+// }
+
+// for (let i = 5; i > 0 ; i-- ){
+//     let row = "";
+    
+//     for(let j = 1; j <= 5 - i; j++){
+//         row += " ";
+//     }
+
+//     for(let j = 1; j <= i; j++){
+//         row += "*";
+//     }
+
+//     console.log(row);
+// }
+
+// let i = 1;
+
+// while(i <= 5){
+    
+//     let row = "";
+
+
+//     let space = 1;
+//     while(space <= 5 - i){
+//         row = row + " ";
+//         space++;
+//     }
+
+//     let star = 1;
+//     while(star <= i){
+//         row = row + "*";
+//         star++;
+//     }
+
+//     console.log(row);
+//     i++;
+// }
+
+// for (let i = 1; i <= 5; i++){
+//     let row = "";
+
+//     // space
+//     for(let j = 1; j <= 5-i; j++){
+//         row += " "
+//     }
+
+//     // star 
+//     for(let j = 1; j <= 2*i -1; j++){
+//         row += "*"
+//     }
+    
+//     console.log(row);
+// }
+
+
+
+// for(let i = 4; i > 0; i-- ){
+//     let row = "";
+
+//     for(let j = 1; j <= 5 - i; j++){
+//         row += " ";
+//     }
+
+//     for(let j = 1; j <= 2*i - 1; j++){
+//         row += "*"
+//     }
+//     console.log(row);
+// }
+
+
+// let i = 5;
+
+// while(i > 0){
+//     let row = "";
+
+//     let space = 1;
+//     while(space <= 5 - i){
+//         row += " ";
+//         space++;
+//     }
+
+//     let star = 1;
+//     while(star <= 2*i -1){
+//         row += "*";
+//         star++;
+//     }
+//     console.log(row);
+//     i--;
+// }
+// let i = 1;
+// while(i <= 5){
+//     let row = "";
+    
+//     let space = 1;
+//     while(space <= 5 - i){
+//         row += " ";
+//         space++;
+//     }
+
+//     let star = 1;
+//     while(star <= 2*i - 1){
+//         row += "*";
+//         star++;
+//     }
+
+//     console.log(row);
+//     i++;
+// }
+
+// for(let i = 1; i<= 5; i++){
+//     let row = "";
+
+//     // Space
+//     for(let s = 1; s <= 5 - i; s++){
+//         row += " ";
+//     }
+
+//     for(let j = 1; j <= 2*i -1; j++){
+//         if(j === 1 || j === 2*i - 1 || i === 5){
+//             row += "*";
+//         }else{
+//             row += " ";
+//         }
+//     }
+//     console.log(row);
+// }
+
+// let i = 1;
+
+// while(i <= 5){
+//     let row = "";
+
+//     // space
+//     let s = 1;
+//     while(s <= 5 - i){
+//         row += " ";
+//         s++;
+//     }
+
+//     // star
+//     let j = 1;
+//     while(j <= 2*i - 1){
+//         if(j === 1 || j === 2*i -1 || i === 5){
+//             row += "*";
+//         }else{
+//             row += " ";
+//         }
+            
+//         j++;
+//     }
+
+//     console.log(row);
+//     i++;
+// }
+
+// for (i = 1; i <= 5; i++){
+//     let row = "";
+//     for(j = 1; j <= 9; j++){
+//         if(i === 1 || i === 5 || j === 1 || j === 9){
+//             row += "*";
+//         }else{
+//             row += " ";
+//         }
+//     }
+//     console.log( i + ":" + row);
+// }
